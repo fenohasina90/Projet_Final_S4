b4131508953b29fa4bff2b5ca8230c1c8088d338
FySamiah <fifaramorasata@gmail.com>
2025-07-07 20:47:06 +0300

    Fond

--- ws/index.php
+++ ws/index.php
@@ -1,6 +1,11 @@
 <?php
 require 'vendor/autoload.php';
 require 'db.php';
+
+$db = getDB(); // Connexion globale à la base
+
 require 'routes/etudiant_routes.php';
+require 'routes/fonds_routes.php';
+require 'routes/comptes_routes.php';
 
 Flight::start();
